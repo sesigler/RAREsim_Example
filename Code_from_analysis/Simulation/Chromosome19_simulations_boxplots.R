375c03b1acb3d970ba9a48a358c2ed2b738f7830
sesigler <sageess98@gmail.com>
2023-09-19 16:30:51 -0500

    update parameter names

--- Code_from_analysis/Simulation/Chromosome19_simulations_boxplots.R
+++ Code_from_analysis/Simulation/Chromosome19_simulations_boxplots.R
@@ -23,8 +23,10 @@ Pop1 = 'AFR'
 Pop2 = 'NFE'
 Nsim = 20000
 p_case = 120
-p_conf1 = 99
-p_conf2 = 80
+p_conf_99 = 99
+p_conf_95 = 95
+p_conf_90 = 90
+p_conf_80 = 80
 bl <- 37  #block with median number of bases
 
 dir_in <- 'C:/Users/sagee/Documents/GitHub/masters_project/'
@@ -38,45 +40,66 @@ exp_mac = data.frame(matrix(nrow = 7, ncol = 9))
 # Load in observed data (RAREsim)
 fun_pcase = read.table(paste0(dir_in, 'obs_MACbin_fun', p_case, '.csv'), header = T, sep = ',')
 fun_exp = read.table(paste0(dir_in, 'obs_MACbin_fun100.csv'), header = T, sep = ',')
-fun_pconf1 = read.table(paste0(dir_in, 'obs_MACbin_fun', p_conf1, '.csv'), header = T, sep = ',')
-fun_pconf2 = read.table(paste0(dir_in, 'obs_MACbin_fun', p_conf2, '.csv'), header = T, sep = ',')
+fun_pconf_99 = read.table(paste0(dir_in, 'obs_MACbin_fun', p_conf_99, '.csv'), header = T, sep = ',')
+fun_pconf_95 = read.table(paste0(dir_in, 'obs_MACbin_fun', p_conf_95, '.csv'), header = T, sep = ',')
+fun_pconf_90 = read.table(paste0(dir_in, 'obs_MACbin_fun', p_conf_90, '.csv'), header = T, sep = ',')
+fun_pconf_80 = read.table(paste0(dir_in, 'obs_MACbin_fun', p_conf_80, '.csv'), header = T, sep = ',')
 syn_exp = read.table(paste0(dir_in, 'obs_MACbin_syn100.csv'), header = T, sep = ',')
-syn_pconf1 = read.table(paste0(dir_in, 'obs_MACbin_syn', p_conf1, '.csv'), header = T, sep = ',')
-syn_pconf2 = read.table(paste0(dir_in, 'obs_MACbin_syn', p_conf2, '.csv'), header = T, sep = ',')
+syn_pconf_99 = read.table(paste0(dir_in, 'obs_MACbin_syn', p_conf_99, '.csv'), header = T, sep = ',')
+syn_pconf_95 = read.table(paste0(dir_in, 'obs_MACbin_syn', p_conf_95, '.csv'), header = T, sep = ',')
+syn_pconf_90 = read.table(paste0(dir_in, 'obs_MACbin_syn', p_conf_90, '.csv'), header = T, sep = ',')
+syn_pconf_80 = read.table(paste0(dir_in, 'obs_MACbin_syn', p_conf_80, '.csv'), header = T, sep = ',')
 
 # Load in expected data (gnomAD)
 exp_fun_case = read.table(paste0(dir_in, 'MAC_bin_estimates_', Nsim, "_", Pop2, '_fun_', p_case,  '.txt'), header=T, sep='\t')
 exp_fun = read.table(paste0(dir_in, 'MAC_bin_estimates_', Nsim, "_", Pop2, '_fun_100.txt'), header=T, sep='\t')
-exp_fun_conf1 = read.table(paste0(dir_in, 'MAC_bin_estimates_', Nsim, "_", Pop2, '_fun_', p_conf1, '.txt'), header=T, sep='\t')
-exp_fun_conf2 = read.table(paste0(dir_in, 'MAC_bin_estimates_', Nsim, "_", Pop2, '_fun_', p_conf2, '.txt'), header=T, sep='\t')
+exp_fun_conf_99 = read.table(paste0(dir_in, 'MAC_bin_estimates_', Nsim, "_", Pop2, '_fun_', p_conf_99, '.txt'), header=T, sep='\t')
+exp_fun_conf_95 = read.table(paste0(dir_in, 'MAC_bin_estimates_', Nsim, "_", Pop2, '_fun_', p_conf_95, '.txt'), header=T, sep='\t')
+exp_fun_conf_90 = read.table(paste0(dir_in, 'MAC_bin_estimates_', Nsim, "_", Pop2, '_fun_', p_conf_90, '.txt'), header=T, sep='\t')
+exp_fun_conf_80 = read.table(paste0(dir_in, 'MAC_bin_estimates_', Nsim, "_", Pop2, '_fun_', p_conf_80, '.txt'), header=T, sep='\t')
 exp_syn = read.table(paste0(dir_in, 'MAC_bin_estimates_', Nsim, "_", Pop2, '_syn_100.txt'), header=T, sep='\t')
-exp_syn_conf1 = read.table(paste0(dir_in, 'MAC_bin_estimates_', Nsim, "_", Pop2, '_syn_', p_conf1, '.txt'), header=T, sep='\t')
-exp_syn_conf2 = read.table(paste0(dir_in, 'MAC_bin_estimates_', Nsim, "_", Pop2, '_syn_', p_conf2, '.txt'), header=T, sep='\t')
+exp_syn_conf_99 = read.table(paste0(dir_in, 'MAC_bin_estimates_', Nsim, "_", Pop2, '_syn_', p_conf_99, '.txt'), header=T, sep='\t')
+exp_syn_conf_95 = read.table(paste0(dir_in, 'MAC_bin_estimates_', Nsim, "_", Pop2, '_syn_', p_conf_95, '.txt'), header=T, sep='\t')
+exp_syn_conf_90 = read.table(paste0(dir_in, 'MAC_bin_estimates_', Nsim, "_", Pop2, '_syn_', p_conf_90, '.txt'), header=T, sep='\t')
+exp_syn_conf_80 = read.table(paste0(dir_in, 'MAC_bin_estimates_', Nsim, "_", Pop2, '_syn_', p_conf_80, '.txt'), header=T, sep='\t')
 
 ### CREATE TABLE of avg num of varints per bin for each pruning step
-avg_vars <- data.frame(matrix(nrow = 7, ncol = 7))
+avg_vars <- data.frame(matrix(nrow = 11, ncol = 7))
 colnames(avg_vars) <- c('Singletons', 'Doubletons', 'MAC=3-5', 'MAC=6-10', 'MAC=11-20',
                        'MAC=21-MAF=0.5%', 'MAF=0.5%-1%')
-rownames(avg_vars) <- c('RAREsim functional-120%', 'RAREsim functional-100%', 
-                        'RAREsim functional-99%', 'RAREsim functional-80%',
-                        'RAREsim synonymous-100%', 'RAREsim synonymous-99%',
-                        'RAREsim synonymous-80%')
+rownames(avg_vars) <- c('RAREsim functional-120%', 'RAREsim functional-100%', 'RAREsim functional-99%',
+                        'RAREsim functional-95%', 'RAREsim functional-90%', 'RAREsim functional-80%',
+                        'RAREsim synonymous-100%', 'RAREsim synonymous-99%', 'RAREsim synonymous-95%', 
+                        'RAREsim synonymous-90%', 'RAREsim synonymous-80%')
 avg_vars[1, ] <- lapply(fun_pcase[, 1:7], mean)
 avg_vars[2, ] <- lapply(fun_exp[, 1:7], mean)
-avg_vars[3, ] <- lapply(fun_pconf1[, 1:7], mean)
-avg_vars[4, ] <- lapply(fun_pconf2[, 1:7], mean)
-avg_vars[5, ] <- lapply(syn_exp[, 1:7], mean)
-avg_vars[6, ] <- lapply(syn_pconf1[, 1:7], mean)
-avg_vars[7, ] <- lapply(syn_pconf2[, 1:7], mean)
+avg_vars[3, ] <- lapply(fun_pconf_99[, 1:7], mean)
+avg_vars[4, ] <- lapply(fun_pconf_95[, 1:7], mean)
+avg_vars[5, ] <- lapply(fun_pconf_90[, 1:7], mean)
+avg_vars[6, ] <- lapply(fun_pconf_80[, 1:7], mean)
+avg_vars[7, ] <- lapply(syn_exp[, 1:7], mean)
+avg_vars[8, ] <- lapply(syn_pconf_99[, 1:7], mean)
+avg_vars[9, ] <- lapply(syn_pconf_95[, 1:7], mean)
+avg_vars[10, ] <- lapply(syn_pconf_90[, 1:7], mean)
+avg_vars[11, ] <- lapply(syn_pconf_80[, 1:7], mean)
+
+avg_vars_tpose <- data.frame(t(avg_vars))
+colnames(avg_vars_tpose) <- rownames(avg_vars)
 
 fwrite(avg_vars, paste0(dir_out, 'avg_variants_per_MACbin.csv'), quote=F, row.names=T, col.names=T, sep=',')
+fwrite(avg_vars_tpose, paste0(dir_out, 'avg_variants_per_MACbin_tpose.csv'), quote=F, row.names=T, col.names=T, sep=',')
 
-exp_all <- cbind(exp_fun_case, exp_fun[, 3], exp_fun_conf1[, 3], exp_fun_conf2[, 3],
-                 exp_syn[, 3], exp_syn_conf1[, 3], exp_syn_conf2[, 3])
+exp_all <- cbind(exp_fun_case, exp_fun[, 3], exp_fun_conf_99[, 3], exp_fun_conf_95[, 3],
+                 exp_fun_conf_90[, 3], exp_fun_conf_80[, 3], exp_syn[, 3], exp_syn_conf_99[, 3], 
+                 exp_syn_conf_95[, 3], exp_syn_conf_90[, 3], exp_syn_conf_80[, 3])
+
 colnames(exp_all) <- c('Lower', 'Upper', 'Expected_var-Fun 120%', 'Expected_var-Fun 100%',
-                       'Expected_var-Fun 99%', 'Expected_var-Fun 80%','Expected_var-Syn 100%',
-                       'Expected_var-Syn 99%', 'Expected_var-Syn 80%')
+                       'Expected_var-Fun 99%', 'Expected_var-Fun 95%', 'Expected_var-Fun 90%',
+                       'Expected_var-Fun 80%','Expected_var-Syn 100%', 'Expected_var-Syn 99%',
+                       'Expected_var-Syn 95%', 'Expected_var-Syn 90%', 'Expected_var-Syn 80%')
+
 fwrite(exp_all, paste0(dir_out, 'exp_variants_per_MACbin_20000_NFE_all.csv'), quote=F, row.names=F, col.names=T, sep=',')
+##################
 
 # Form the data frames for the observed and expected counts
 obs_mac <- rbind(fun_pcase, fun_exp, fun_pconf1, fun_pconf2, syn_exp, syn_pconf1, syn_pconf2)
@@ -90,13 +113,14 @@ exp_mac[1:7, 1:7] <- rbind(exp_fun_case$Expected_var, exp_fun$Expected_var,
 colnames(exp_mac) <- colnames(obs_mac)
 exp_mac$rep <- '.'
 exp_mac$data <- c('gnomAD functional-120%', 'gnomAD functional-100%', 
-                  'gnomAD functional-99%', 'gnomAD functional-80%',
-                  'gnomAD synonymous-100%', 'gnomAD synonymous-99%',
-                  'gnomAD synonymous-80%')
+                  paste0('gnomAD functional-', p_conf1, '%'), 
+                  paste0('gnomAD functional-', p_conf2, '%'),
+                  'gnomAD synonymous-100%', paste0('gnomAD synonymous-', p_conf1, '%'),
+                  paste0('gnomAD synonymous-', p_conf2, '%'))
 
 # Combine the observed and expected data into one dataframe
-melted_obs <- melt(obs_mac, id = c('data', 'rep'))
-melted_exp <- melt(exp_mac, id = c('data', 'rep'))
+melted_obs <- reshape2::melt(obs_mac, id = c('data', 'rep'))
+melted_exp <- reshape2::melt(exp_mac, id = c('data', 'rep'))
 
 # setwd('/Users/megansorenson/Documents/Package/RAREsim_Example/Code_from_analysis/Simulation/')
 # setwd('C:/Users/sagee/Documents/GitHub/RAREsim_Example/Code_from_analysis/Simulation/')
@@ -183,8 +207,8 @@ syn100 <- ggplot(subset(melted_obs, data %in% 'RAREsim synonymous-100%'),
   theme(plot.margin = unit(c(0.2,0.5,0.1,0.2), "cm"))
 syn100
 
-# Functional-99%
-fun99 <- ggplot(subset(melted_obs, data %in% paste0('RAREsim functional-', p_conf1, '%')), 
+# Functional-p_conf1 %
+fun_p_conf1 <- ggplot(subset(melted_obs, data %in% paste0('RAREsim functional-', p_conf1, '%')), 
                  aes(x=variable, y=value , col = data)) +
   geom_boxplot() + 
   labs(title = paste0('Pruning: ', p_conf1, '% Functional'), 
@@ -197,10 +221,10 @@ fun99 <- ggplot(subset(melted_obs, data %in% paste0('RAREsim functional-', p_con
   scale_color_manual(values=cols_fun) +
   # guides(color = guide_legend(override.aes=list(shape = c(8, 1, 4, 8, 8, 8)))) +
   theme(plot.margin = unit(c(0.2,0.5,0.1,0.2), "cm"))
-fun99
+fun_p_conf1
 
-# Synonymous-99%
-syn99 <- ggplot(subset(melted_obs, data %in% paste0('RAREsim synonymous-', p_conf1, '%')), 
+# Synonymous-p-conf1 %
+syn_p_conf1 <- ggplot(subset(melted_obs, data %in% paste0('RAREsim synonymous-', p_conf1, '%')), 
                  aes(x=variable, y=value , col = data)) +
   geom_boxplot() + 
   labs(title = paste0('Pruning: ', p_conf1, '% Synonymous'), 
@@ -213,10 +237,10 @@ syn99 <- ggplot(subset(melted_obs, data %in% paste0('RAREsim synonymous-', p_con
   scale_color_manual(values=cols_syn) +
   # guides(color = guide_legend(override.aes=list(shape = c(8, 1, 4, 8, 8, 8)))) +
   theme(plot.margin = unit(c(0.2,0.5,0.1,0.2), "cm"))
-syn99
+syn_p_conf1
 
-# Functional-80%
-fun80 <- ggplot(subset(melted_obs, data %in% paste0('RAREsim functional-', p_conf2, '%')), 
+# Functional-p_conf2 %
+fun_p_conf2 <- ggplot(subset(melted_obs, data %in% paste0('RAREsim functional-', p_conf2, '%')), 
                 aes(x=variable, y=value , col = data)) +
   geom_boxplot() + 
   labs(title = paste0('Pruning: ', p_conf2, '% Functional'), 
@@ -229,10 +253,10 @@ fun80 <- ggplot(subset(melted_obs, data %in% paste0('RAREsim functional-', p_con
   scale_color_manual(values=cols_fun) +
   # guides(color = guide_legend(override.aes=list(shape = c(8, 1, 4, 8, 8, 8)))) +
   theme(plot.margin = unit(c(0.2,0.5,0.1,0.2), "cm"))
-fun80
+fun_p_conf2
 
-# Synonymous-80%
-syn80 <- ggplot(subset(melted_obs, data %in% paste0('RAREsim synonymous-', p_conf2, '%')), 
+# Synonymous-p_conf2 %
+syn_p_conf2 <- ggplot(subset(melted_obs, data %in% paste0('RAREsim synonymous-', p_conf2, '%')), 
                 aes(x=variable, y=value , col = data)) +
   geom_boxplot() + 
   labs(title = paste0('Pruning: ', p_conf2, '% Synonymous'), 
@@ -245,7 +269,7 @@ syn80 <- ggplot(subset(melted_obs, data %in% paste0('RAREsim synonymous-', p_con
   scale_color_manual(values=cols_syn) +
   # guides(color = guide_legend(override.aes=list(shape = c(8, 1, 4, 8, 8, 8)))) +
   theme(plot.margin = unit(c(0.2,0.5,0.1,0.2), "cm"))
-syn80
+syn_p_conf2
 
 ### SAVE THE PLOTS ###
 # Extract the legend for each pruning scenario
@@ -262,12 +286,14 @@ both100 <- ggplot(subset(melted_obs, data %in% c('RAREsim functional-100%', 'RAR
   theme(legend.position = "bottom") +
   theme(plot.margin = unit(c(0.2,0.5,0.1,0.2), "cm"))
 
-both99 <- ggplot(subset(melted_obs, data %in% c('RAREsim functional-99%', 'RAREsim synonymous-99%')), 
+both_p_conf1 <- ggplot(subset(melted_obs, data %in% c(paste0('RAREsim functional-', p_conf1, '%'), 
+                                                      paste0('RAREsim synonymous-', p_conf1, '%'))), 
                   aes(x=variable, y=value , col = data)) +
   geom_boxplot() + 
   labs(y = 'Number of Variants', x = 'MAC Bins') +
   theme(axis.text.x = element_text(angle = 35, hjust=0.65)) +
-  geom_point(data=subset(melted_exp, data %in% c('gnomAD functional-99%', 'gnomAD synonymous-99%')),
+  geom_point(data=subset(melted_exp, data %in% c(paste0('gnomAD functional-', p_conf1, '%'), 
+                                                 paste0('gnomAD synonymous-', p_conf1, '%'))),
              aes(x=variable, y=value), shape  = 8) +
   theme(axis.title.x = element_blank()) +
   theme(axis.title.y = element_blank()) +
@@ -275,12 +301,14 @@ both99 <- ggplot(subset(melted_obs, data %in% c('RAREsim functional-99%', 'RAREs
   theme(legend.position = "bottom") +
   theme(plot.margin = unit(c(0.2,0.5,0.1,0.2), "cm"))
 
-both80 <- ggplot(subset(melted_obs, data %in% c('RAREsim functional-80%', 'RAREsim synonymous-80%')), 
+both_p_conf2 <- ggplot(subset(melted_obs, data %in% c(paste0('RAREsim functional-', p_conf2, '%'),
+                                                      paste0('RAREsim synonymous-', p_conf2, '%'))), 
                  aes(x=variable, y=value , col = data)) +
   geom_boxplot() + 
   labs(y = 'Number of Variants', x = 'MAC Bins') +
   theme(axis.text.x = element_text(angle = 35, hjust=0.65)) +
-  geom_point(data=subset(melted_exp, data %in% c('gnomAD functional-80%', 'gnomAD synonymous-80%')),
+  geom_point(data=subset(melted_exp, data %in% c(paste0('gnomAD functional-', p_conf2, '%'),
+                                                 paste0('gnomAD synonymous-', p_conf2, '%'))),
              aes(x=variable, y=value), shape  = 8) +
   theme(axis.title.x = element_blank()) +
   theme(axis.title.y = element_blank()) +
@@ -289,8 +317,8 @@ both80 <- ggplot(subset(melted_obs, data %in% c('RAREsim functional-80%', 'RAREs
   theme(plot.margin = unit(c(0.2,0.5,0.1,0.2), "cm"))
 
 leg100 <- g_legend(both100)
-leg99 <- g_legend(both99)
-leg80 <- g_legend(both80)
+leg_p_conf1 <- g_legend(both_p_conf1)
+leg_p_conf2 <- g_legend(both_p_conf2)
 
 # Combine the Functional and Synonymous plots into one figure
 pruned100 <- grid.arrange(arrangeGrob(fun100 + theme(legend.position="none"),
@@ -304,27 +332,27 @@ pruned100 <- grid.arrange(arrangeGrob(fun100 + theme(legend.position="none"),
 ggsave(file = paste0(dir_out, 'obs_v_exp_MACbins_', Pop2, '_fun100_syn100.jpg'),
        plot = pruned100, height = 8, width = 12, units = 'in')
 
-pruned99 <- grid.arrange(arrangeGrob(fun99 + theme(legend.position="none"),
-                                     syn99 + theme(legend.position="none"),
-                                     nrow=1),
-                          leg99, nrow=2, heights = c(10, 1),
+pruned_p_conf1 <- grid.arrange(arrangeGrob(fun_p_conf1 + theme(legend.position="none"),
+                                           syn_p_conf1 + theme(legend.position="none"),
+                                           nrow=1),
+                          leg_p_conf1, nrow=2, heights = c(10, 1),
                           left = 'Number of Variants',
                           top = textGrob('100% NFE: Observed vs Expected Number of Variants per MAC bin',
                                          gp = gpar(fontsize = 18, font = 1)))
 # Save the plot
 ggsave(file = paste0(dir_out, 'obs_v_exp_MACbins_', Pop2, '_fun', p_conf1, '_syn', p_conf1, '.jpg'),
-       plot = pruned99, height = 8, width = 12, units = 'in')
+       plot = pruned_p_conf1, height = 8, width = 12, units = 'in')
 
-pruned80 <- grid.arrange(arrangeGrob(fun80 + theme(legend.position="none"),
-                                     syn80 + theme(legend.position="none"),
-                                     nrow=1),
-                         leg80, nrow=2, heights = c(10, 1),
+pruned_p_conf2 <- grid.arrange(arrangeGrob(fun_p_conf2 + theme(legend.position="none"),
+                                           syn_p_conf2 + theme(legend.position="none"),
+                                           nrow=1),
+                         leg_p_conf2, nrow=2, heights = c(10, 1),
                          left = 'Number of Variants',
                          top = textGrob('100% NFE: Observed vs Expected Number of Variants per MAC bin',
                                         gp = gpar(fontsize = 18, font = 1)))
 # Save the plot
 ggsave(file = paste0(dir_out, 'obs_v_exp_MACbins_', Pop2, '_fun', p_conf2, '_syn', p_conf2, '.jpg'),
-       plot = pruned80, height = 8, width = 12, units = 'in')
+       plot = pruned_p_conf2, height = 8, width = 12, units = 'in')
 
 
 # for(bl in c(56,66)){ # 5th and 95th percentile for number of bases
